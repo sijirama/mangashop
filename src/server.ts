51f3af654bd8a15b1be713a147206e93df4622b5
sijibomi <gbemilesanmi@gmail.com>
2023-04-06 23:45:44 +0100

    file convention

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,28 @@
-console.log(process.env.AUTHOR)
+import Express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = Express()
+const PORT = process.env.PORT || 5173
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT ,  () => {
+  console.log(`Server running on Port:${PORT}`)
+});
