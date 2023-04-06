76c325e2e05d34ae5f957c70ad2b2c7ee85b009f
sijibomi <gbemilesanmi@gmail.com>
2023-04-07 00:59:14 +0100

    middleware and router initialization

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import Express from "express";
 import dotenv from "dotenv";
 
+//Import Routes
 import { MangaRoute } from "./routes/manga";
 import { RootRoute } from "./routes/root";
 
@@ -11,12 +12,10 @@ const PORT = process.env.PORT || 5173;
 
 
 //////////////////////////////MIDDLEWARE
+app.use(Express.urlencoded({ extended: false }));  // built-in middleware to handle urlencoded form data
+app.use(Express.json());  // built-in middleware for json
 
-// built-in middleware to handle urlencoded form data
-app.use(Express.urlencoded({ extended: false }));
 
-// built-in middleware for json
-app.use(Express.json());
 
 ////////////////////////////////ROUTES
 app.get("/", RootRoute);
